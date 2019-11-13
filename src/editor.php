336fdb00917cee4380103dd343942eb7525b144a
Reka <reka37@mail.ru>
2019-11-13 22:09:41 +0300

    finalcomposer

---
+++ src/editor.php
@@ -0,0 +1,27 @@
+<?php
+namespace irek\editor;
+class Edit {
+	public function run(){
+		echo '<div id="quillEditor" style="height:300px"></div>';
+	}
+}
+?>
+<script src="https://code.jquery.com/jquery-latest.js"></script>
+<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+<script>
+	$(document).ready(function(){
+		var options = {
+			modules: {
+			toolbar: [
+			  ['bold', 'italic'],
+			  ['link'],
+			  [{ list: 'ordered' }, { list: 'bullet' }]
+			]
+		  },
+		  placeholder: 'Введите сообщение',
+		  theme: 'snow'
+		};
+		var editor = new Quill('#quillEditor', options);
+	});
+</script>
